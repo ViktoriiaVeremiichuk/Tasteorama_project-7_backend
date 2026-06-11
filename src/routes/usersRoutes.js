30602c4e115e8f2422aaad4f9f58bfed89433b5d
AnnaMinnegalieva <minnegalieva.anna@gmail.com>
2026-06-12 01:35:27 +0300

    add usersRoutes

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,7 +1,11 @@
+
 import { Router } from 'express';
+import { getCurrentUser } from '../controllers/usersController.js';
+//import { authenticate } from '../middleware/authenticate.js';
 
 const router = Router();
 
-
+// router.get('/current', authenticate, getCurrentUser);
+router.get('/current', getCurrentUser);
 
 export default router;
