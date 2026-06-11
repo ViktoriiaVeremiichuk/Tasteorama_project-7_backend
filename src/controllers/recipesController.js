43e98dfdded98a00064bc08641e0ea7027e47fe5
Anastasia <anastasiapanasian@gmail.com>
2026-06-12 00:24:13 +0300

    backend-favorites-na

---
+++ src/controllers/recipesController.js
@@ -0,0 +1,14 @@
+import Recipe from "../models/recipe.js";
+
+export const getFavoriteRecipes = async (req, res, next) => {
+    try {
+          
+    const userId = req.user._id;
+
+    const favoriteRecipes = await Recipe.find({ favorites: userId });
+
+    return res.status(200).json(favoriteRecipes);
+  } catch (error) {
+    next(error);
+  }
+}
--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+import { getFavoriteRecipes } from "../controllers/recipesController.js";
+import { authenticate } from "../middlewares/authenticate.js";
 
 const router = Router();
 
-
+router.get("/favorite", authenticate,  celebrate(getFavoriteSchema), 
+  getFavoriteRecipes);
 
 export default router;
