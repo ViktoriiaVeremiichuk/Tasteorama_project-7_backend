95e5505234d8102ff5ee3a931e1b7a3de8d0a119
Vladimir_Kostik <vladimirkostik48@gmail.com>
2026-06-12 19:09:07 +0300

    work in progress

--- src/controllers/recipesController.js
+++ src/controllers/recipesController.js
@@ -1,6 +1,26 @@
+import { Recipe } from "../models/recipe.js";
+
 export const addToFavoritesController = async (
   req,
   res,
 ) => {
-  // TODO
+  const { recipeId } = req.params;
+
+  const recipe = await Recipe.findById(recipeId);
+
+  if (!recipe) {
+    return res.status(404).json({
+      message: "Recipe not found",
+    });
+  }
+
+  // TODO:
+  // отримати користувача після реалізації authenticate middleware
+  // перевірити чи рецепт вже є в favorites
+  // додати рецепт до favorites
+  // зберегти користувача
+
+  res.status(200).json({
+    message: "Controller stub is ready",
+  });
 };
