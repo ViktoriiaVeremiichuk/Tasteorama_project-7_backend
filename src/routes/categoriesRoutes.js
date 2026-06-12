9ef6a4e7920580757be72acd0cd3c4c956113368
Olga Magnusson <olga.stolyrchuk@gmail.com>
2026-06-12 15:00:05 +0200

    add categories endpoint

--- src/routes/categoriesRoutes.js
+++ src/routes/categoriesRoutes.js
@@ -3,6 +3,6 @@ import { getCategories } from '../controllers/categoriesController.js';
 
 const router = Router();
 
-router.get("/api/categories", getCategories);
+router.get("/", getCategories);
 
 export default router;
