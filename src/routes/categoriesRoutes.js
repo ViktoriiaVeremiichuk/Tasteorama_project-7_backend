916d026983bebb4b348380ae37416e0eb700dc9b
Olga Magnusson <olga.stolyrchuk@gmail.com>
2026-06-12 14:46:20 +0200

    create controller categories and routes

--- src/routes/categoriesRoutes.js
+++ src/routes/categoriesRoutes.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
+import { getCategories } from '../controllers/categoriesController.js';
 
 const router = Router();
 
-
+router.get("/api/categories", getCategories);
 
 export default router;
