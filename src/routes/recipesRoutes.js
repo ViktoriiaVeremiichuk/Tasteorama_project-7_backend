5718497b8ac090692d3ab0334f8a1000ad093fbf
TaTiaNaM <maluchenko.tania@gmail.com>
2026-06-13 20:26:21 +0300

    del

--- src/routes/recipesRoutes.js
+++ src/routes/recipesRoutes.js
@@ -7,7 +7,7 @@ import { recipeIdParamSchema } from '../validation/recipesValidation.js';
 const router = Router();
 
 router.post("/favorites/:recipeId", authenticate, addFavoriteRecipe);
-router.delete("/api/recipes/favorites/:recipeId", celebrate(recipeIdParamSchema), authenticate, deleteRecipe);
+router.delete("/favorites/:recipeId", celebrate(recipeIdParamSchema), authenticate, deleteRecipe);
 
 export default router;
 
