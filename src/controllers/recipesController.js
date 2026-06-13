fac50f935def5fce5cac190bc88fa0aaa013d40a
Anastasia <anastasiapanasian@gmail.com>
2026-06-13 19:23:19 +0300

    backend-favorites-na

--- src/controllers/recipesController.js
+++ src/controllers/recipesController.js
@@ -63,3 +63,21 @@ export const getOwnRecipes = async (req, res, next) => {
     next(err);
   }
 };
+
+
+export const getFavoriteRecipes = async (req, res, next) => {
+  try {
+    const userId = req.user._id;
+
+
+    const userWithFavorites = await User.findById(userId).populate("favorites");
+
+    if (!userWithFavorites) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.status(200).json(userWithFavorites.favorites);
+  } catch (error) {
+    next(error);
+  }
+};
