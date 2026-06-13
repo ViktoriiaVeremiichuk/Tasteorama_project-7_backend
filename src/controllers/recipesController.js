f0f4ed1d2978899dafacf303c17e3a6681a714e0
Tetiana Zinovieva <talia.97.it@gmail.com>
2026-06-13 16:27:25 +0200

    add readable

--- src/controllers/recipesController.js
+++ src/controllers/recipesController.js
@@ -1,4 +1,23 @@
+import { Readable } from "stream";
+import cloudinary from "../utils/cloudinary.js";
 import { Recipe } from "../models/recipe.js";
+
+const uploadToCloudinary = (buffer) => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      {
+        folder: "tasteorama-recipes",
+      },
+      (error, result) => {
+        if (error) return reject(error);
+
+        resolve(result);
+      }
+    );
+
+    Readable.from(buffer).pipe(stream);
+  });
+};
 
 export const createRecipe = async (req, res, next) => {
   try {
@@ -10,7 +29,15 @@ export const createRecipe = async (req, res, next) => {
       time,
       calories,
       ingredients,
-    } = req.body;
+      } = req.body;
+      
+      let thumb = "";
+
+    if (req.file) {
+    const uploadedImage = await uploadToCloudinary(req.file.buffer);
+
+    thumb = uploadedImage.secure_url;
+    }
 
     const recipe = await Recipe.create({
       title,
@@ -25,7 +52,7 @@ export const createRecipe = async (req, res, next) => {
           ? JSON.parse(ingredients)
           : ingredients,
 
-      thumb: req.file?.path || "",
+      thumb,
 
       owner: req.user._id,
     });
