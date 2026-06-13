a30aae58e3a15ce37a7d89e52141c31bdc35c20b
AnnaMinnegalieva <minnegalieva.anna@gmail.com>
2026-06-13 18:35:21 +0300

    fix getCurrentUser controller

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,10 +1,20 @@
-export const getCurrentUser = async (req, res) => {
-  const { _id, name, email, avatar } = req.user;
+import createHttpError from 'http-errors';
 
-  res.json({
-    _id,
-    name,
-    email,
-    avatar,
-  });
+export const getCurrentUser = async (req, res, next) => {
+  try {
+    if (!req.user) {
+      throw createHttpError(401, 'Not authenticated');
+    }
+
+    const { _id, name, email, avatar } = req.user;
+
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      avatar,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
