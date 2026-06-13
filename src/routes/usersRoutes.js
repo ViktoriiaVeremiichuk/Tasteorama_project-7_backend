8753136803dc394b7e48d4f016ae89876565ed30
AnnaMinnegalieva <minnegalieva.anna@gmail.com>
2026-06-13 14:44:39 +0300

    add auth to users/current

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,11 +1,10 @@
 
 import { Router } from 'express';
 import { getCurrentUser } from '../controllers/usersController.js';
-//import { authenticate } from '../middleware/authenticate.js';
+import { authenticate } from '../middleware/authenticate.js';
 
 const router = Router();
 
-// router.get('/current', authenticate, getCurrentUser);
-router.get('/current', getCurrentUser);
+router.get('/current', authenticate, getCurrentUser);
 
 export default router;
