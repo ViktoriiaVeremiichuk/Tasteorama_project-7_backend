3687bdf8bedc68de33a16a7fd92696f3584748c7
Karina Bosenko <karina.bosenko.10.10@gmail.com>
2026-06-13 13:32:26 +0300

    Fix favorites endpoint validation and error handling

--- src/controllers/recipesController.js
+++ src/controllers/recipesController.js
@@ -1,24 +1,34 @@
+import mongoose from "mongoose";
 import createHttpError from "http-errors";
 import { Recipe } from "../models/recipe.js";
 import { User } from "../models/user.js";
 
-export const addFavoriteRecipe = async (req, res) => {
-  const { recipeId } = req.params;
+export const addFavoriteRecipe = async (req, res, next) => {
+  try {
+    const { recipeId } = req.params;
 
-  const recipe = await Recipe.exists({ _id: recipeId });
+    if (!mongoose.isValidObjectId(recipeId)) {
+      throw createHttpError(400, "Invalid recipe ID format");
+    }
 
-  if (!recipe) {
-    throw createHttpError(404, "Recipe not found");
-  }
+    const recipe = await Recipe.exists({ _id: recipeId });
 
-  const user = await User.findByIdAndUpdate(
-    req.user._id,
-    {
-      $addToSet: {
-        favorites: recipeId,
+    if (!recipe) {
+      throw createHttpError(404, "Recipe not found");
+    }
+
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $addToSet: {
+          favorites: recipeId,
+        },
       },
-    },
-    { new: true },
-  );
-  res.status(200).json({ favorites: user.favorites });
+      { new: true },
+    );
+
+    res.status(200).json({ favorites: user.favorites });
+  } catch (error) {
+    next(error);
+  }
 };
