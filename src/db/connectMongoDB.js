3cb1e5c6c97bd956d8acbce426a0f0220d1fcbc2
Vladimir_Kostik <vladimirkostik48@gmail.com>
2026-06-13 09:25:53 +0300

    fix mongodb dns resolution

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -1,14 +1,13 @@
-import mongoose from 'mongoose';
+import dns from "dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
+import mongoose from "mongoose";
 
 export async function connectMongoDB() {
   try {
-    const mongoUrl = process.env.MONGO_URL;
-    console.log("MONGO_URL =", mongoUrl);
-    await mongoose.connect(mongoUrl);
-    console.log('✅ MongoDB connection established successfully');
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("✅ MongoDB connection established successfully");
   } catch (error) {
-    console.error(error.message);
-    process.exit(1);
+    console.error(error);
   }
 }
