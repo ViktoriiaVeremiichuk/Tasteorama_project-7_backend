04dd986dc5b35055ec5172fb3144ae81a47ab343
TaTiaNaM <maluchenko.tania@gmail.com>
2026-06-13 13:00:17 +0300

    private

--- src/routes/recipesRoutes.js
+++ src/routes/recipesRoutes.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import { celebrate } from "celebrate";
 import { deleteRecipe } from '../controllers/recipesController.js';
+import { authenticate } from "../middleware/authenticate.js";
+import {recipeIdParamSchema} from "../validations/recipesValidation.js";
 
 const router = Router();
 
-router.delete("/api/recipes/favorites/:recipeId", deleteRecipe);
+router.delete("/api/recipes/favorites/:recipeId", celebrate(recipeIdParamSchema), authenticate, deleteRecipe);
 
 export default router;
