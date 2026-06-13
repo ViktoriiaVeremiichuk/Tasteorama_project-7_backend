11d1b565d2b34fc528afd35e70b810c962411de0
Tetiana Zinovieva <talia.97.it@gmail.com>
2026-06-13 16:13:57 +0200

    add route

--- src/routes/recipesRoutes.js
+++ src/routes/recipesRoutes.js
@@ -1,7 +1,20 @@
-import { Router } from 'express';
+import { Router } from "express";
+
+import { authenticate } from "../middleware/authenticate.js";
+import { upload } from "../middleware/upload.js";
+
+import { createRecipe } from "../controllers/recipesController.js";
+
+import { createRecipeSchema } from "../validation/recipesValidation.js";
 
 const router = Router();
 
-
+router.post(
+  "/",
+  authenticate,
+  upload.single("thumb"),
+  createRecipeSchema,
+  createRecipe
+);
 
 export default router;
