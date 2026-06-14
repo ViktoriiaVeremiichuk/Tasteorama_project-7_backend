01d56e17b564dc1f969b657b74decac904076c93
TaTiaNaM <maluchenko.tania@gmail.com>
2026-06-14 17:47:19 +0300

    res

--- src/validation/recipesValidation.js
+++ src/validation/recipesValidation.js
@@ -1,23 +1,7 @@
 import { Joi, Segments } from 'celebrate';
-import { isValidObjectId } from 'mongoose';
 
-export const recipeIdParamSchema ={
+export const recipeIdParamSchema = {
   [Segments.PARAMS]: Joi.object({
-    id: Joi.string().required(),
-    measure: Joi.string().required(),
-    title: Joi.string().required(),
-    category: Joi.string().required(),
-    owner: Joi.string().required(),
-    area: Joi.string(),
-    instructions: Joi.string().required(),
-    description: Joi.string(),
-    thumb: Joi.string(),
-    time: Joi.number().required(),
-    calories: Joi.number(),
-    ingredients: Joi.array().items(Joi.object({
-      id: Joi.string().required(),
-      measure: Joi.string().required(),
-    })),
-    timestamps: Joi.date(),
-  })
+    recipeId: Joi.string().required(),
+  }),
 };
