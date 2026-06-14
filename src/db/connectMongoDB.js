05fd520859410198942d1ad0e2bac107c8788ffe
Vladimir_Kostik <vladimirkostik48@gmail.com>
2026-06-14 22:50:45 +0300

    add swagger documentation setup

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -1,9 +1,14 @@
+import dns from "dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 import mongoose from "mongoose";
 
 export async function connectMongoDB() {
   try {
     const mongoUrl = process.env.MONGO_URL;
+
     await mongoose.connect(mongoUrl);
+
     console.log("✅ MongoDB connection established successfully");
   } catch (error) {
     console.error(error.message);
